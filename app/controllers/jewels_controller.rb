4e53b56a780f740f0bc3b05c68cb4eb7aceff89d
Sacha Gotainer <sgotainer@hotmail.com>
2022-08-22 17:58:10 +0200

    Added new and create methods and new view

--- app/controllers/jewels_controller.rb
+++ app/controllers/jewels_controller.rb
@@ -1,5 +1,5 @@
 class JewelsController < ApplicationController
-  skip_before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @jewels = Jewel.all
@@ -9,9 +9,23 @@ class JewelsController < ApplicationController
     @jewel = Jewel.find(params[:id])
   end
 
+  def new
+    @jewel = Jewel.new
+  end
+
+  def create
+    @jewel = Jewel.new(jewels_params)
+    @jewel.user = current_user
+    if @jewel.save
+      redirect_to jewel_path(@jewel)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def jewels_params
-    params.require(:jewel).permit(:name, :price_per_day, :photo, :brand, :category)
+    params.require(:jewel).permit(:name, :price_per_day, :brand, :category, :user_id, :comment, photos: [])
   end
 end
