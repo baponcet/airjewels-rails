780e8ea29713fc3cfcc2b0e49b48131343ceaa39
Fariha Shah fariha@shahinvest.co <fariha@shahinvest.co>
2022-08-22 17:29:02 +0200

    booking controller and view-new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,23 @@
 class BookingsController < ApplicationController
   skip_before_action :authenticate_user!
 
-
   def new
+    @jewel = Jewel.find(params[:jewel_id])
+    @user = current_user
+    @booking = Booking.new
   end
 
   def create
+    @jewel = Jewel.find(params[:jewel_id])
+    @booking = Booking.new(booking_params)
+    @user = current_user
+    @booking.user = @user
+    @booking.jewel = @jewel
+    @booking.save!
+    redirect_to jewel_path(@jewel), notice: "Booking validated"
+  end
+
+  def booking_params
+    params.require(:booking).permit(:starting_date, :ending_date)
   end
 end
