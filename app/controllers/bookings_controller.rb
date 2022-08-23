c9b089114ec4cf38c58f34ce6005ad2f32ca585f
Fariha Shah fariha@shahinvest.co <fariha@shahinvest.co>
2022-08-23 17:26:54 +0200

    jewel-view-show jewel-controller-error-clean booking-controller-new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,20 +14,14 @@ class BookingsController < ApplicationController
     @user = current_user
     @booking.user = @user
     @booking.jewel = @jewel
-<<<<<<< HEAD
-<<<<<<< HEAD
     @booking.save!
     redirect_to jewel_path(@jewel), notice: "Booking validated"
-=======
-=======
     authorize @booking
->>>>>>> 47d90922c2ae0ea7ebfc84cc5b1e3a1ee728b408
     if @booking.save
       redirect_to jewel_path(@jewel), notice: "Congrats ! Your booking is validated"
     else
       render :new, status: :unprocessable_entity
     end
->>>>>>> 0ea2b8a7906ec309be1b9a53c1cb75f7f5e005e3
   end
 
   def booking_params
