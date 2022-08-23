187390b42635e5858f082b369f32a8910a72d39c
Sebastien VILLEBRUN <sebastien.villebrun@gmail.com>
2022-08-23 13:11:14 +0200

    correction on validations with rails 7

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,15 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :starting_date, :ending_date, presence: true
-  validate :end_date_after_start_date
-
-  private
-
-  def end_date_after_start_date
-    return if ending_date.blank? || starting_date.blank?
-
-    if ending_date < starting_date
-      errors.add(:end_date, "Must be after the start date")
-    end
-  end
+  validates_comparison_of :ending_date, greater_than: :starting_date
 end
