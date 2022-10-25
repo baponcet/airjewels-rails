870d359772630e3d2a076219baa6bb0f05753c40
Sebastien VILLEBRUN <sebastien.villebrun@gmail.com>
2022-10-25 12:41:11 +0200

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def configure_permitted_parameters
-    attributes = [:first_name, :last_name, :email, :password, :password_confirmation]
+    attributes = %i[first_name last_name email password password_confirmation]
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
 
@@ -19,5 +19,4 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-
 end
